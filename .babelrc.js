6fc7ceefbc4faccbe8e7a93ac5566c0c2d6deb73
Neil Zeng <neilz@synnex.com>
2020-05-06 22:24:41 +0800

    init: summit template

---
+++ .babelrc.js
@@ -0,0 +1,48 @@
+const presets = [
+  [
+    "@babel/preset-env",
+    {
+      targets: {
+        chrome: "58",
+        ie: "11"
+      },
+      useBuiltIns: "usage",
+      corejs: "core-js@3"
+    }
+  ],
+  "@babel/preset-react",
+  "@babel/preset-typescript",
+];
+const plugins = [
+  [
+    "@babel/plugin-proposal-decorators",
+    {
+      legacy: true
+    }
+  ],
+  "@babel/plugin-proposal-class-properties",
+  "@babel/plugin-syntax-dynamic-import",
+  [
+    "@babel/plugin-transform-runtime",
+    {
+      helpers: false,
+      regenerator: true,
+      corejs: 2
+    }
+  ]
+];
+// if (process.env["BABEL_ENV"] && process.env["BABEL_ENV"] === "production") {
+//   plugins.push(
+//     [
+//       "@babel/plugin-transform-modules-systemjs",
+//       {
+//         // outputs SystemJS.register(...)
+//         "systemGlobal": "SystemJS"
+//       }
+//     ]
+//   )
+// }
+module.exports = {
+  presets,
+  plugins
+};
---
+++ webpack/prod.config.js
@@ -0,0 +1,78 @@
+const path = require("path");
+const glob = require("glob");
+// const HtmlPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const common = require("./common");
+
+const paths = {
+    root: path.join(__dirname, '..'),
+    src: path.join(__dirname, '..', 'src'),
+    dist: path.join(__dirname, '..', 'dist'),
+};
+
+const getParentDirName = (filePath) => {
+    const b = path.resolve(filePath, "../../");
+    const c = path.resolve(filePath, "../");
+    return path.basename(c.replace(b, ""));
+}
+
+const entry = (() => {
+    const entryComponentsFiles = [
+      ...(glob.sync(`${paths.src}/components/**/index.tsx`) || []),
+    ];
+    const entryJsMap = {};
+
+    entryComponentsFiles.forEach(filepath => {
+      const fileName = path.basename(filepath, '.tsx').toLocaleLowerCase();
+      const parentName = getParentDirName(filepath);
+      entryJsMap[`${parentName}/${fileName}`] = filepath;
+    });
+    return { entryJsMap };
+})();
+
+module.exports = {
+  mode: "production",
+
+  entry: entry.entryJsMap,
+  
+  output: {
+    path: paths.dist,
+    filename: '[name].js',
+    libraryTarget: 'system',
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[name].css"
+    }),
+
+    new CopyWebpackPlugin(
+      common.copyPluginConfig.patterns,
+      common.copyPluginConfig.options
+    ),
+  ],
+
+  module: {
+    rules: [
+      common.jsLoader,
+      common.fileLoader,
+      common.urlLoader,
+      common.lessLoader,
+    ]
+  },
+
+  // externals: {
+  //   'moment': 'moment',
+  //   'react': 'React',
+  //   'react-dom': 'ReactDOM'
+  // },
+
+  resolve: {
+    ...common.resolve
+  }
+};
